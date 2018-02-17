4c6e89a08d3c5de8d7abfcc304c41f7eae43789f
Angel Del Castillo <jubu201504@gmail.com>
2018-02-16 22:00:20 -0600

    Updated CI.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+## DeGOps: 0.0.4
 set -o errexit
 set -o nounset
 
---
+++ split.go
@@ -0,0 +1,13 @@
+package zipf
+
+// SplitWord return any valid word inside s.
+func SplitWord(s string) ([]string, error) {
+	// TODO; implement.
+	return nil, nil
+}
+
+// SplitSymbol return any non-valid char inside s.
+func SplitSymbol(s string) ([]string, error) {
+	// TODO; implement.
+	return nil, nil
+}
